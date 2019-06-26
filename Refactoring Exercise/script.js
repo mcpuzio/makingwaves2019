a78511cd603d6f678ee255c616ee5b7f029c6b10
Mike Puzio <mcpuzio@gmail.com>
2019-06-26 20:06:04 +0200

    Total Smoothie Delight Project + Refactoring Assignment
    All files and assets which are to provide the ‘Smoothie Delight’ client
    project and the Refactoring Assignment also given.

---
+++ Refactoring Exercise/script.js
@@ -0,0 +1,6 @@
+function myFunction() {if(document.getElementById('see-list').style.visibility === 'hidden') {
+          document.getElementById('see-list').style.visibility = 'visible';
+      } else {
+          document.getElementById('see-list').style.visibility = 'hidden';
+      }
+  }
